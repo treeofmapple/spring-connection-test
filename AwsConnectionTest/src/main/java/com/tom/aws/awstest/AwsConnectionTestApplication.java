8be04e1682c778896cc55c55ce29280ee29ca8ee
Samuel Pereira <samuelsantana778@gmail.com>
2025-03-15 07:48:21 -0300

    first commit

---
+++ AwsConnectionTest/src/main/java/com/tom/aws/awstest/AwsConnectionTestApplication.java
@@ -0,0 +1,13 @@
+package com.tom.aws.awstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsConnectionTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsConnectionTestApplication.class, args);
+	}
+
+}
---
+++ AwsConnectionTest/src/test/java/com/tom/aws/awstest/AwsConnectionTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tom.aws.awstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsConnectionTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
