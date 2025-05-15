5061e4805a3dc7ec63ce0a686d7c9270651be920
Samuel Pereira <samuelsantana778@gmail.com>
2025-05-15 08:37:47 -0300

    package management

--- Main/src/main/java/com/tom/aws/awstest/AwsConnectionTestApplication.java
+++ Main/src/main/java/com/tom/aws/awstest/AwsConnectionTestApplication.java
@@ -2,10 +2,16 @@ package com.tom.aws.awstest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableJpaRepositories(
+		excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX, pattern = "com.tom.aws.awstest.image.oldTag.*")
+)
 public class AwsConnectionTestApplication {
 
 	public static void main(String[] args) {
--- Main/src/main/java/com/tom/aws/awstest/imagetag/ImageTag.java
+++ Main/src/main/java/com/tom/aws/awstest/imagetag/ImageTag.java
@@ -1,6 +1,7 @@
 package com.tom.aws.awstest.imagetag;
 
 import com.tom.aws.awstest.image.Image;
+import com.tom.aws.awstest.models.Auditable;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -13,24 +14,26 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 @Table(name = "image_tag")
-public class ImageTag {
+public class ImageTag extends Auditable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     
-    @Column(nullable = false, unique = true)
+    @Column(name = "category", nullable = false, unique = true)
     private String category;
 
-    @Column(nullable = true, unique = false)
-    private String subcategory;
+    @Column(name = "sub_category", nullable = true, unique = false)
+    private String subCategory;
 	
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "image_id", nullable = true)
